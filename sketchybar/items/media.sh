d4da551923c66f7d98ace679589a350d1bbf6dfe
RAJ <bibhanshuraj19@gmail.com>
2024-12-24 18:00:16 +0530

    sketchybar_folder

---
+++ sketchybar/items/media.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sketchybar --add item media center \
+           --set media label.color=$ACCENT_COLOR \
+                       label.max_chars=20 \
+                       icon.padding_left=0 \
+                       scroll_text=on \
+                       icon=􀑪 \
+                       icon.color=$ACCENT_COLOR \
+                       background.drawing=off \
+                       script="$PLUGIN_DIR/media.sh" \
+           --subscribe media media_change
+
